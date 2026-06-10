17c03eafdb212043d5233e8a329ca3edda7edfdd
TATA Gianluca <Gianluca.TATA@akkodis.com>
2026-06-10 19:57:29 +0200

    DEL : Spostato libDeck in nuovo progetto 'game-lb'

--- cpp_lib/html/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tokendeck_0',['TokenDeck',['../class_fate_bag_1_1_token_deck.html',1,'FateBag::TokenDeck'],['../class_fate_bag_1_1_token_deck.html#ae7650c33f6e86c855f7b47368295c72c',1,'FateBag::TokenDeck::TokenDeck()']]]
-];
